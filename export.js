01390d42ee47dbe1d44ea66512e58494c67af6d1
Tommy <54972963+EWCMF@users.noreply.github.com>
2020-09-09 10:45:18 +0200

    test multiple imports

---
+++ export.js
@@ -0,0 +1,10 @@
+function hurra() {
+    console.log("hurra")
+}
+
+function hest() {
+    console.log("hest")
+}
+
+exports.hest = hest
+exports.hurra = hurra
---
+++ import.js
@@ -0,0 +1,4 @@
+var dt = require('./export.js');
+
+dt.hest()
+dt.hurra()
